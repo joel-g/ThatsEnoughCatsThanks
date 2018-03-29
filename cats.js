438eba77cd525f935c355f2ea1a3117d217ae168
Joel Guerra <joelaguerra1@gmail.com>
2018-03-28 21:52:00 -0700

    adds subs to list

--- cats.js
+++ cats.js
@@ -1,3 +1,41 @@
+// https://www.reddit.com/r/help/comments/44498b/how_to_hide_or_filter_a_subreddit_from_rall/#res:settings/filteReddit
 
 
-$('.addRowButton:eq(1)').trigger('click');
+const subreddits = [
+  'cats',
+  'aww',
+  'jellybeantoes',
+  'catsinsinks',
+  'meow_irl',
+  'startledcats',
+  'catsonglass',
+  'catvideos',
+  'catpictures',
+  'kittens',
+  'catloaf',
+  'catsstandingup',
+  'blep',
+  'tuckedinkitties',
+  'stuffoncats',
+  'catgifs',
+  'floof',
+  'catpranks',
+  'catsinbusinessattire',
+  'lolcats',
+  'catreactiongifs',
+  'catbellies',
+  'midlystartledcats',
+  'cathighfive',
+  'catreddit',
+  'catcircles',
+  'catsvstechnology',
+]
+
+
+for (let i = 0; i < subreddits.length; i++) {
+  $('.addRowButton:eq(1)').trigger('click');
+  $("#subreddits_filteRedditSubredditsSubreddits_" + (i+1)).val(subreddits[i])
+}
+
+
+
